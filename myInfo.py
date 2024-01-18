b815587f5fc5a9255deba80822320878d6ab56ac
s1360844 <146123883+s1360844@users.noreply.github.com>
2024-01-18 12:58:40 -0500

    my information v2

--- myInfo.py
+++ myInfo.py
@@ -5,3 +5,4 @@
 print ("My name is: Jared Mishen")
 print ("My address is: 145 Giegerich Avenue, Staten Island, NY 10307")
 print ("My phone number is: 917-829-1038")
+print ("My college major is: A Bachelor of Science in Computer Science")
