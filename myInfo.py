a6e35c8a6557486fd9ab970d29448df7c8b57da5
s1360844 <146123883+s1360844@users.noreply.github.com>
2024-01-18 12:56:12 -0500

    my information v1

---
+++ myInfo.py
@@ -0,0 +1,7 @@
+# CS175L
+# Jared Mishen
+# myInfo
+
+print ("My name is: Jared Mishen")
+print ("My address is: 145 Giegerich Avenue, Staten Island, NY 10307")
+print ("My phone number is: 917-829-1038")
