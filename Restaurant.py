f570b4fab0352f403f08c6030802799c1ba798e9
s1360844 <146123883+s1360844@users.noreply.github.com>
2024-02-12 19:37:52 -0500

    Add files via upload

--- Restaurant.py
+++ Restaurant.py
@@ -3,31 +3,45 @@
 #Restaurant
 
 
-vegetarian = input("Is anyone in your party vegetarian? ".lower())
-vegan = input("Is anyone in your party vegan? ".lower())
-gluten_free = input("Is anyone in your party gluten-free? ".lower())
-print("Here are your restaurant choices.")
-
-if(vegetarian == "yes") and (vegan == "no") and (gluten_free == "yes"):
-    print("Main Street Pizza Company")
-    print("Corner Café")
-    print("The Chef’s Kitchen")
-
-elif(vegetarian == "yes") and (vegan == "no") and (gluten_free == "no"):
-    print("Main Street Pizza")
-    print("Mama's Fine Italian")
-    print("Corner Café")
-    print("The Chef’s Kitchen")
-
-elif(vegetarian == "no") and (vegan == "no") and (gluten_free == "no"):
-    print("Joe’s Gourmet Burgers")
-    print("Main Street Pizza Company")
-    print("Mama’s Fine Italian")
-    print("Corner Café")
-    print("The Chef’s Kitchen")
-
-else:
-    print("Corner Café")
-    print("The Chef’s Kitchen")
+keep_going = 'yes'
 
  
+
+
+while keep_going == 'yes':
+    vegetarian = input("Is anyone in your party vegetarian? ".lower())
+    vegan = input("Is anyone in your party vegan? ".lower())
+    gluten_free = input("Is anyone in your party gluten-free? ".lower())
+    print("Here are your restaurant choices.")
+
+    if(vegetarian == "yes") and (vegan == "no") and (gluten_free == "yes"):
+        print("Main Street Pizza Company")
+        print("Corner Café")
+        print("The Chef’s Kitchen")
+
+    elif(vegetarian == "yes") and (vegan == "no") and (gluten_free == "no"):
+        print("Main Street Pizza")
+        print("Mama's Fine Italian")
+        print("Corner Café")
+        print("The Chef’s Kitchen")
+
+    elif(vegetarian == "no") and (vegan == "no") and (gluten_free == "no"):
+        print("Joe’s Gourmet Burgers")
+        print("Main Street Pizza Company")
+        print("Mama’s Fine Italian")
+        print("Corner Café")
+        print("The Chef’s Kitchen")
+
+
+    else:
+        print("Corner Café")
+        print("The Chef’s Kitchen")
+
+ 
+
+    keep_going = input("Enter 'yes' if you would like to do another restaurant search (enter 'no' to end): ")
+
+   
+
+ 
+
