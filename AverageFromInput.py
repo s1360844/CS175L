ed5883f9e5ea2ab97646f1f64b2d74ef0bb2cf98
s1360844 <146123883+s1360844@users.noreply.github.com>
2024-03-18 11:25:35 -0400

    Add files via upload

---
+++ AverageFromInput.py
@@ -0,0 +1,23 @@
+# CS 175L
+# Jared Mishen
+# AverageFromInput
+
+def main():
+    total = 0
+    count = 0
+
+    with open('numbers.txt', 'r') as file:
+        for line in file:
+            number = int(line)
+            count += 1
+            total += number
+            print(f"I read in {count} number(s) Current number is: {number} Total is: {total}")
+
+    if count > 0:
+        average = total / count
+        print(f"Average: {average}")
+    else:
+        print("No numbers found in the file.")
+
+if __name__ == "__main__":
+    main()
